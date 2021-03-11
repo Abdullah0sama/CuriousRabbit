b6d5d412bf1fec8cc1561f9ff62f52649fb04b2a
ao <abdullahosama15@gmail.com>
2021-03-11 23:07:29 +0200

    Installing BodyParser

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost/CuriousRabbitDB", {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true})
     .then(() => console.log("Connected Successfully to db"))
     .catch((err) => console.log(err));
 
+
 const User = require("./models/user.js");
 const Question = require("./models/question.js");
 
 
+var questionsRouter = require("./routes/questions.js");
+app.use(questionsRouter);
 
 
 
