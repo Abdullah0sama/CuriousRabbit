c079ff32140c3157ccd3e2d41acc12c02b6b6ddb
ao <abdullahosama15@gmail.com>
2021-03-19 21:18:03 +0200

    Authorization check

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
-
+const { session } = require("passport");
+const passport = require("passport");
 const saltRounds = 10;
 
 const User = require("../models/user");
@@ -21,4 +22,39 @@ router.post("/register", async function(req, res){
 
 })
 
+router.post("/login",passport.authenticate('local', {failureRedirect:"/login"}), function(req, res){
+    console.log(req.body);
+    res.send("done");
+    console.log(req.user);
+});
+
+router.get("/secret", isAuthenticated, function(req, res){
+    res.send("this is protected");
+})
+router.get("/", function(req, res){
+    console.log(req.session);
+    res.send("dione");
+})
+//Checks if users are Authenticated if not redirects them to login page
+function isAuthenticated(req, res, next){
+    if(!req.isAuthenticated()) {
+            res.redirect("/login");
+    }else{
+        next();
+    }
+}
+
+//Checks if users are unAuthenticated if not redirects them to index page
+function isUnauthenticated(req, res, next){
+    if(req.isAuthenticated()) {
+            res.redirect("/");
+    }else{
+        next();
+    }
+}
+// Ends user's session
+router.post("/logout", function(req, res){
+    req.logOut();
+    res.redirect("/");
+})
 module.exports = router;
