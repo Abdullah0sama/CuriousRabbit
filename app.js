b65c3b20ee8f0016d77fdf78fcc53a525527486a
ao <abdullahosama15@gmail.com>
2021-03-19 19:08:59 +0200

    Setting up authentication

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const express           =   require("express");
+const app               =   express();
+const mongoose          =   require("mongoose");
+const bodyParser        =   require("body-parser");
+const passport          =   require("passport");
+const localStrategy     =  require("passport-local").Strategy;
+
+const session_secret = process.env.CR_session;
+const session = require("express-session");
+app.use(session({
+    secret: session_secret,
+    name: 'curiousrabbit',
+    saveUninitialized: false,
+    resave: true
+}));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+const bcrypt = require("bcrypt");
 
 mongoose.connect("mongodb://localhost/CuriousRabbitDB", {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true})
-    .then(() => console.log("Connected Successfully to db"))
-    .catch((err) => console.log(err));
-
+.then(() => console.log("Connected Successfully to db"))
+.catch((err) => console.log(err));
 
 const User = require("./models/user.js");
 const Question = require("./models/question.js");
+
+
+// authentication 
+passport.use(new localStrategy(
+    function(username, password, done){
+        return User.findOne({username: username}).then( usr => {
+            if(!usr) return done(null, false);
+            if(!bcrypt.compareSync(password, usr.password)) return done(null, false);
+            return done(null, usr);
+        }).catch(err => {return done(err)});
+    }
+));
+    
+passport.serializeUser(function(usr, done){
+    done(null, usr._id);
+});
+passport.deserializeUser(function(id, done){
+    User.findById(id).then(usr => done(null, usr))
+    .catch(err => done(err));
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 
 let questionsRouter = require("./routes/questions.js");
